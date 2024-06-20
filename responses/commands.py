4a309a9b624380021d2d874b937978de532aba33
Zeptiny <arturtrentinibenedetti@gmail.com>
2024-06-20 19:20:00 -0300

    Add responses

---
+++ responses/commands.py
@@ -0,0 +1,5 @@
+commmandsList = {
+    "hello": "Hello there!",
+    "bye": "Goodbye!",
+    "ping": "Pong!",
+}
---
+++ responses/errors.py
@@ -0,0 +1,5 @@
+responses = {
+    "The received string length is longer than maximum allowed": "Você, ou o servidor, contém um nickname, scoreboard, tag, ou similar, com um tamanho maior do que o permitido.",
+    "Unable to access jarfile": "Não foi possível encontrar o arquivo de inicialização (Jar) do seu servidor, verifique o nome nos parâmetros de inicialização",
+    "but we require at least": "A versão do java utilizada pelo seu servidor é superior a configurada"
+}
