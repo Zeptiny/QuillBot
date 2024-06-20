ffb7736b99200cfd04da688ee179d5fcb3161f73
Zeptiny <arturtrentinibenedetti@gmail.com>
2024-06-20 20:09:27 -0300

    Adição de !plo e !plano

--- responses/commands.py
+++ responses/commands.py
@@ -1,5 +1,5 @@
 commmandsList = {
-    "hello": "Hello there!",
-    "bye": "Goodbye!",
+    "plo": "**Plano**: Quais recursos procura \n**Localização**: Onde \n**Orçamento**: Quanto está disposto pagar \n \nSe está em dúvida em qual plano precise, de !plano",
+    "plano": "**Versão**: Qual a versão do servidor \n**Players**: Quantidade de players simultaneos \n**Mods/Plugins**: Quantos mods/plugins (Especifique) \n**Modo**: Qual o modo de jogo do server",
     "ping": "Pong!",
 }
