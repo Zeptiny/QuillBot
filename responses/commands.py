38cbdbda639f12c7e2386c25f5f9c06c51dffd36
Grentenville <arturtrentinibenedetti@gmail.com>
2024-06-21 18:15:34 -0300

    Mudança plo para plov

--- responses/commands.py
+++ responses/commands.py
@@ -1,4 +1,4 @@
 commmandsList = {
-    "plo": "**Plano**: Quais recursos procura \n**Localização**: Onde \n**Orçamento**: Quanto está disposto pagar \n \nSe está em dúvida em qual plano precise, de !plano",
+    "plov": "**P**lano/**P**layers: Quais recursos procura \n**L**ocalização: Onde \n**O**rçamento: Quanto está disposto pagar \n**V**ersão: Qual a versão do servidor\n \nSe está em dúvida em qual plano precise, de !plano",
     "plano": "**Versão**: Qual a versão do servidor \n**Players**: Quantidade de players simultaneos \n**Mods/Plugins**: Quantos mods/plugins (Especifique) \n**Modo**: Qual o modo de jogo do server"
 }
